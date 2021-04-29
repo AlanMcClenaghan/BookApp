1a894d04cf169220006f89057c20f143a45cd0a2
Alan McClenaghan <alandmcclenaghan@icloud.com>
2021-04-29 13:53:02 +0100

    Added for:each template to create layout items.

--- force-app/main/default/lwc/bookApp/bookApp.js
+++ force-app/main/default/lwc/bookApp/bookApp.js
@@ -3,22 +3,21 @@ const BOOK_URL = 'https://www.googleapis.com/books/v1/volumes?q=';
 
 export default class BookApp extends LightningElement {
 
-    query = 'cats';
+    query = 'Man';
     books;
 
     connectedCallback() {
         this.fetchBookData();
     }
 
-    fetchBookData() {
-        fetch(BOOK_URL + this.query)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            this.books = data;
+    fetchBookData(){
+        fetch(BOOK_URL+this.query)
+        .then(response=>response.json())
+        .then(data=>{
+            console.log(data)
+            this.books = data
         })
-        .catch(error => console.error(error))
+        .catch(error=>console.error(error))
     }
-
 
 }
